fd96ed5b3440f2d33aab071362bebdff8094ec69
kike09v <44410377+kike09v@users.noreply.github.com>
2021-01-04 18:14:09 -0400

    Primer commit

---
+++ config/yargs.js
@@ -0,0 +1,29 @@
+const descripcion =  {
+    demand:true,
+    alias: 'd',
+    desc: 'Descripción de la tarea por hacer'
+};
+
+const completado = {
+    default: true,
+    alias: 'c',
+    desc: 'Marca como completado o pendiente la tarea'
+}
+
+const argv = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', {
+        descripcion
+    })
+    .command('borrar', 'Borrar un elemento', {
+        descripcion
+    })
+    .command('actualizar', 'Actualizar el estado completado de una tarea', {
+        descripcion,
+        completado
+    })
+    .help()
+    .argv;
+
+    module.exports = {
+        argv
+    }
